35d4489a819981be61fda4a93b072a60722397e2
Rina Razumov <rinash3@gmail.com>
2019-07-08 17:27:44 -0400

    updated files

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ var path = require ("path");
 var app = express();
 var PORT = process.env.PORT|| 3002;
 
+app.use(express.static(path.join(__dirname, './app/public')));
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 // Starts the server to begin listening
 // =============================================================
