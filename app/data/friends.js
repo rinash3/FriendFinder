6215bc6d2c04b693b56423c73f458015b94bcaac
Rina Razumov <rinash3@gmail.com>
2019-07-09 22:39:58 -0400

    final updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,16 @@ var friends= [
     "name":"Ahmed",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[5,1,4,4,5,1,2,5,4,1]
+},
+{
+  "name":"test",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[5,1,4,4,5,1,2,5,4,1]
+},
+{
+  "name":"test2",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[5,3,4,4,5,3,2,5,4,3]
 }
 ]
   // Note how we export the array. This makes it accessible to other files using require.
